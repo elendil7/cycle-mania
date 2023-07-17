c5a599882ca1e73786ef1416574a544a17067645
Aragorn <80429382+AragornElessar1973@users.noreply.github.com>
2023-07-17 23:29:22 +0100

    Add some initial command handling

--- src/Discord/Events/InteractionCreate.ts
+++ src/Discord/Events/InteractionCreate.ts
@@ -1,11 +1,21 @@
 import { Events, Interaction } from "discord.js";
 import DiscordBot from "../Structures/DiscordBot";
 import CommandHandler from "../Handlers/CommandHandler";
+import CooldownHandler from "../Handlers/CooldownHandler";
 
 export default {
   name: Events.InteractionCreate,
   once: false,
   async run(client: DiscordBot, interaction: Interaction) {
+    // if command was not run in a guild, return
+    if (!interaction.guild) return;
+
+    // avoid handling any other command besides slash commands
+    if (!interaction.isChatInputCommand()) return;
+
+    // run cooldown handler
+    await CooldownHandler(client, interaction);
+
     // run command handler
     await CommandHandler(client, interaction);
   },
