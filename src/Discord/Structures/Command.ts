5162fc7fafa77be7a280d79916c0531e1194ac77
Aragorn <80429382+AragornElessar1973@users.noreply.github.com>
2023-07-17 23:30:35 +0100

    Add info field to all cmds, & fix types

--- src/Discord/Structures/Command.ts
+++ src/Discord/Structures/Command.ts
@@ -1,11 +1,16 @@
 import DiscordBot from "./DiscordBot";
 import { CommandInteraction, SlashCommandBuilder } from "discord.js";
+import ICommandInfo from "../Types/ICommandInfo";
 
-export default interface SlashCommand {
-  data: SlashCommandBuilder;
+// main command interface for slash commands
+interface SlashCommand {
+  data: Omit<SlashCommandBuilder, "addSubcommand" | "addSubcommandGroup">;
+  info?: ICommandInfo;
   execute(
     client: DiscordBot,
     interaction: CommandInteraction,
     args?: any[],
   ): any;
 }
+
+export default SlashCommand;
