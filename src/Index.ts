357f7e0d780a7b1be7bb8e96e991a12f378b1c5e
Aragorn <80429382+AragornElessar1973@users.noreply.github.com>
2023-07-18 04:29:43 +0100

    Export vital services as singletons

--- src/Index.ts
+++ src/Index.ts
@@ -3,14 +3,27 @@ import * as dotenv from "dotenv";
 import { resolve } from "path";
 dotenv.config({ path: resolve(__dirname, "..", ".env") });
 
+import PuppeteerService from "./Services/PuppeteerService";
 import StravaService from "./Services/StravaService";
 import DiscordService from "./Services/DiscordService";
+
+// variables
+let puppeteerService: PuppeteerService;
+let stravaService: StravaService;
+let discordService;
 
 // self executing function
 (async function () {
   console.log("Starting services...");
 
   // start services, sequentially
-  const stravaService = new StravaService();
-  const discordService = new DiscordService();
+  puppeteerService = new PuppeteerService();
+  stravaService = new StravaService();
+  discordService = new DiscordService();
+
+  // purge cache (based on services no longer in use; derived from directory names)
+  await puppeteerService.purgeCache();
 })();
+
+// export certain services as singletons
+export { puppeteerService, stravaService };
