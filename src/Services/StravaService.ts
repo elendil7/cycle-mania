7fd75976db3d52ce59b794c958eae228f34c818e
Aragorn <80429382+AragornElessar1973@users.noreply.github.com>
2023-07-18 04:30:22 +0100

    Setup some HTTP reqs to Strava using strava-v3 lib

--- src/Services/StravaService.ts
+++ src/Services/StravaService.ts
@@ -1,23 +1,65 @@
 import {
+  STRAVA_ACCESS_TOKEN,
   STRAVA_CLIENT_ID,
   STRAVA_CLIENT_SECRET,
 } from "../Discord/Utils/exportedEnvs";
 import { config_STRAVA } from "../../config";
+import axios from "axios";
+import SummaryClub from "../API/Strava/v3/models/SummaryClub";
+// import { default as strava, Strava } from "strava-v3";
+const strava = require("strava-v3");
+import { Strava } from "strava-v3";
+import { Symbols } from "../Utils/constants";
+import { puppeteerService } from "../Index";
 
-export default class StavaService {
-  private BASE_URL = config_STRAVA.base_URL;
-  private CLIENT_ID = STRAVA_CLIENT_ID;
-  private CLIENT_SECRET = STRAVA_CLIENT_SECRET;
+export default class StravaService {
+  private client: Strava;
+  private config: typeof config_STRAVA;
+  private CLIENT_ID: string;
+  private CLIENT_SECRET: string;
+  private ACCESS_TOKEN: string;
 
   constructor() {
-    this.testStrava();
+    this.client = strava;
+    this.config = config_STRAVA;
+    this.CLIENT_ID = STRAVA_CLIENT_ID;
+    this.CLIENT_SECRET = STRAVA_CLIENT_SECRET;
+    this.ACCESS_TOKEN = STRAVA_ACCESS_TOKEN;
+    this.init();
   }
 
-  // run test to see if we can get a response from strava
-  private testStrava() {
-    return new Promise((resolve, reject) => {
-      resolve([]);
+  private async init() {
+    // make puppeteer browser
+    // await puppeteerService.launchBrowser(this.constructor.name);
+
+    console.log(`${Symbols.HOURGLASS} Loading Strava config...`);
+    this.client.config({
+      client_id: this.CLIENT_ID,
+      client_secret: this.CLIENT_SECRET,
+      access_token: this.ACCESS_TOKEN,
+      redirect_uri: this.config.redirect_URL,
     });
+    console.log(`${Symbols.CHECKMARK} Loaded!`);
+
+    try {
+      console.log(`${Symbols.HOURGLASS} Pinging strava...`);
+      const response = await axios.get(`${this.config.base_URL}/athlete`, {
+        headers: {
+          Authorization: `Bearer ${this.ACCESS_TOKEN}`,
+        },
+      });
+      if (response.status === 200) {
+        console.log(`${Symbols.CHECKMARK} Ping successful!`);
+      } else {
+        console.error(
+          `${Symbols.FAILURE} Failed to connect to the Strava API. Check refresh token.`,
+        );
+      }
+    } catch (e) {
+      console.error(
+        `${Symbols.FAILURE} Error connecting to to the Strava API. Restart bot.`,
+      );
+    }
   }
 
   public getActivities() {
@@ -39,10 +81,15 @@ export default class StavaService {
   }
 
   // club
-  public getClub() {
-    return new Promise((resolve, reject) => {
-      resolve([]);
-    });
+  public async getClub(clubID: string) {
+    try {
+      return (await this.client.clubs.get({
+        id: clubID,
+        access_token: this.ACCESS_TOKEN,
+      })) as SummaryClub;
+    } catch (e) {
+      console.error(e);
+    }
   }
 
   public getClubActivities() {
@@ -70,10 +117,11 @@ export default class StavaService {
   }
 
   // leaderboard
-  public getClubGroupLeaderboard() {
-    return new Promise((resolve, reject) => {
-      resolve([]);
-    });
+  public async getClubGroupLeaderboard(clubID: string) {
+    try {
+    } catch (e) {
+      console.error(e);
+    }
   }
 
   public getClubSegmentLeaderboard() {
