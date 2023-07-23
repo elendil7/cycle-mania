439df5e2a9d53bbbd750272887eb1a88d9863f4b
Aragorn <80429382+AragornElessar1973@users.noreply.github.com>
2023-07-23 04:07:04 +0100

    Vanity bot startup visualization

--- src/Index.ts
+++ src/Index.ts
@@ -6,27 +6,48 @@ dotenv.config({ path: resolve(__dirname, "..", ".env") });
 import PuppeteerService from "./Services/PuppeteerService";
 import StravaService from "./Services/StravaService";
 import DiscordService from "./Services/DiscordService";
-import { Symbols } from "./Utils/constants";
+import { LineBreak, Symbols } from "./Utils/constants";
 
 // variables
 let puppeteerService: PuppeteerService;
 let stravaService: StravaService;
-let discordService;
+let discordService: DiscordService;
 
-// self executing function
-(async function () {
-  console.log(`${Symbols.INFORMATION} Starting services...`);
+function startServices() {
+  console.log(
+    `\n\x1b[1m<<< ${Symbols.INFORMATION} Starting services... >>>\x1b[0m`,
+  );
+  console.log(LineBreak);
 
   // start services, sequentially
   puppeteerService = new PuppeteerService();
   stravaService = new StravaService();
   discordService = new DiscordService();
 
+  console.log(LineBreak);
+}
+
+async function initializeServices() {
+  console.log(
+    `\x1b[1m<<< ${Symbols.INFORMATION} Initializing services... >>>\x1b[0m`,
+  );
+  console.log(LineBreak);
+
   // purge puppeteer cache (based on services no longer in use; derived from directory names)
   puppeteerService.purgeCache();
+  // attempt ping to strava
+  await stravaService.attemptPing();
+  // start discord bot
+  await discordService.startBot();
 
-  console.log(`${Symbols.SUCCESS} Services started.`);
-})();
+  console.log(LineBreak);
+}
 
-// export certain services as singletons
+// start services
+startServices();
+
+// export certain services as singletons (for use in other files, before services are initialized)
 export { puppeteerService, stravaService };
+
+// initialize services
+initializeServices();
